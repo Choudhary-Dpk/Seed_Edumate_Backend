24aaec15d79fcc20a53d7064b99840c75884d2c3
riyaz <riyaz@seedglobaleducation.com>
2025-09-02 21:30:26 +0530

    changes in prisma models

--- src/models/helpers/auth.ts
+++ src/models/helpers/auth.ts
@@ -5,7 +5,7 @@ import { LoginStatus } from "../../types/auth";
 export const revokePreviousEmailTokens = async (userId: number) => {
   await prisma.token.deleteMany({
     where: {
-      userId: userId,
+      user_id: userId,
     },
   });
 };
@@ -13,7 +13,7 @@ export const revokePreviousEmailTokens = async (userId: number) => {
 export const saveEmailToken = async (userId: number, emailToken: string) => {
   await prisma.token.create({
     data: {
-      userId,
+      user_id: userId,
       token: emailToken,
       createdOn: new Date(),
     },
@@ -24,7 +24,7 @@ export const getUserDetailsFromToken = async (emailToken: string) => {
   const token = await prisma.token.findFirst({
     select: {
       id: true,
-      userId: true,
+      user_id: true,
       user: {
         select: {
           email: true,
@@ -43,7 +43,7 @@ export const getUserDetailsFromToken = async (emailToken: string) => {
 export const useEmailToken = async (userId: number, emailToken: string) => {
   await prisma.token.deleteMany({
     where: {
-      userId: userId,
+      user_id: userId,
       token: emailToken,
     },
   });
@@ -68,7 +68,7 @@ export const updatePassword = async (userId: number, passwordHash: string) => {
 export const deleteOtps = async (userId: number) => {
   await prisma.userOtp.deleteMany({
     where: {
-      userId,
+      user_id: userId,
     },
   });
 };
@@ -76,7 +76,7 @@ export const deleteOtps = async (userId: number) => {
 export const saveOtp = async (userId: number, otp: string) => {
   await prisma.userOtp.create({
     data: {
-      userId,
+      user_id: userId,
       otp: otp,
       createdOn: new Date(),
     },
@@ -86,7 +86,7 @@ export const saveOtp = async (userId: number, otp: string) => {
 export const validateOtp = async (userId: number, otp: string) => {
   const otpData = await prisma.userOtp.findFirst({
     where: {
-      userId,
+      user_id: userId,
       otp,
       createdOn: {
         gte: moment().subtract(5, "minutes").toDate(),
@@ -100,7 +100,7 @@ export const validateOtp = async (userId: number, otp: string) => {
 export const useOtp = async (userId: number, otp: string) => {
   const result = await prisma.userOtp.deleteMany({
     where: {
-      userId,
+      user_id: userId,
       otp,
     },
   });
