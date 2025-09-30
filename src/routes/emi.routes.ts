5c3ce05040ae37176b9a2115e3d676ea52b8c9f3
riyaz <riyaz@seedglobaleducation.com>
2025-09-30 19:09:16 +0530

    added two functions to create lead

---
+++ src/routes/emi.routes.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.post("/createEligibilityCheckerLead");
+router.post("createEmiCalculatorLead",);
+
+export { router as emiRoutes };
