61b323f6502a7807e6a9bef0c9e27bca6896d5b1
riyaz <riyaz@seedglobaleducation.com>
2025-10-01 11:28:05 +0530

    added controllers

--- src/routes/emi.routes.ts
+++ src/routes/emi.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
-
+import {
+  createEligibilityCheckerLead,
+  createEmiCalculatorLead,
+} from "../controllers/emi.controller";
 const router = Router();
 
-router.post("/createEligibilityCheckerLead");
-router.post("createEmiCalculatorLead",);
+router.post("/createEligibilityCheckerLead", createEligibilityCheckerLead);
+router.post("createEmiCalculatorLead", createEmiCalculatorLead);
 
 export { router as emiRoutes };
