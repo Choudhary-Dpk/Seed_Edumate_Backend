a02b39958e8454378e933dbf208a3b07b603feba
riyaz <riyaz@seedglobaleducation.com>
2026-02-02 15:56:28 +0530

    started workers

--- src/app.ts
+++ src/app.ts
@@ -65,5 +65,5 @@ app.listen(PORT, async () => {
   console.log(`Server is running on port ${PORT}`);
   await checkPrismaConnection();
   //  Start background workers
-  // await startWorkers();
+  await startWorkers();
 });
